337101bccc69d9f66597a44ad171db6b921004b4
Lip Han <liphan.pang@gmail.com>
2017-06-05 14:48:40 +0800

    request.file html input type=file

--- application.py
+++ application.py
@@ -269,7 +269,7 @@ def profile():
 
 
         if request.method == "POST":
-            path = request.file['file location']
+            path = request.files['file location']
             # path = request.form['file location']
             ref = request.form['project ref']
             building_name = request.form['building name']
@@ -316,9 +316,9 @@ def profile():
 
     # prepare pandas dataframe for plots
             directory = os.path.dirname(path)
-            # path_ = os.path.join('path')
+            path_ = os.path.join('path')
             # abs_path = os.path.abspath(path_)
-            df = pd.read_csv(path)
+            df = pd.read_csv(path_)
 
             df['date'] = pd.to_datetime(df['date'])
             df['time'] = pd.to_datetime(df['time']).dt.strftime('%H:%M:%S')
